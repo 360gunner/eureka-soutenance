5c656e258d896faec70e9d19f714ef665e94fa97
360gunner <hkentache@gmail.com>
2020-06-16 16:37:30 +0100

    soutenance ELHAMDULILLAH

---
+++ src/main/java/com/example/demo/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//eureka server with dashboard
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
